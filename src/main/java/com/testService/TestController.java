5937335716c5212a85c3a37fa3c807dbd0e9b6d2
Diogo Dutra <dutradda@gmail.com>
2019-08-03 15:19:52 -0300

    corrigindo erros

--- src/main/java/com/testService/TestController.java
+++ src/main/java/com/testService/TestController.java
@@ -4,11 +4,18 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+*
+*/
 @RestController
 @CrossOrigin
 public class TestController {
 
-	@GetMapping("/home")
+    /**
+    *
+    *@return
+    */
+    @GetMapping("/home")
 	public String homeRequest() {
 		return "Hello World!";
 	}
--- src/main/java/com/testService/TestServiceApplication.java
+++ src/main/java/com/testService/TestServiceApplication.java
@@ -1,21 +1,25 @@
 package com.testService;
 
-import com.testService.TestController;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+*
+*/
 @SpringBootApplication
 @ComponentScan(basePackageClasses = { TestController.class })
 @EnableAutoConfiguration
 public class TestServiceApplication {
 
-	private String bar = new String("bar");
-	public static void main(String[] args) {
-		SpringApplication.run(TestServiceApplication.class, args);
-		System.exit(0);
-	}
+    /**
+    *
+    *@param args s
+    */
+    public static void main(final String[] args) {
+        SpringApplication.run(TestServiceApplication.class, args);
+        System.exit(0);
+    }
 
 }
